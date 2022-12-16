37d19ce33411e1bde746c9ae3346d940b94ac399
KimEungChan <dmdcks789@gmail.com>
2022-12-16 13:20:43 +0900

    TodoAppSubmit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+// eslint-disable-next-line import/namespace, import/default, import/no-named-as-default, import/no-named-as-default-member
 import Planer from './components/planer';
-import { useState } from 'react';
 import styled from 'styled-components';
 
-
-
 const Body = styled.body`
-  background-image: url("/static/winter.jpeg");
+  background-image: url('/static/winter.jpeg');
   width: 100vw;
-`
+`;
 
 function App() {
-
   return (
-    <Body >
-      <Planer/>
+    <Body>
+      <Planer />
     </Body>
   );
 }
